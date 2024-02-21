13a188df714090161536e7df9a2b1665defd2642
NRS\nrsha <nrshakib007@gmail.com>
2024-02-21 13:45:40 +0600

    Added Table Columns and Row Data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,30 @@
-const Table = () => {
-  return <div></div>;
+const Table = ({ data }) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>Title</div>
+          <div>Categories</div>
+          <div>Price</div>
+          <div>Date</div>
+          <div>Author</div>
+          <div>Status</div>
+          <div>Action</div>
+        </div>
+        {data.map((rowData, index) => (
+          <div key={index}>
+            <div>{rowData.title}</div>
+            <div>{rowData.categories}</div>
+            <div>{rowData.price}</div>
+            <div>{rowData.date}</div>
+            <div>{rowData.author}</div>
+            <div>{rowData.status}</div>
+            <div>{rowData.action}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Table;
