61ef6e0d1bed24af8432f140c8e85f5fe11dbf05
NRS\nrsha <nrshakib007@gmail.com>
2024-02-22 01:12:08 +0600

    Responsive Design

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -27,44 +27,68 @@ const Table = ({ data }) => {
   };
 
   return (
-    <div className="text-center flex gap-10">
+    <div className="text-center lg:justify-center md:justify-center lg:flex md:flex gap-10 md:gap-0 sm:grid">
       {/* This div is to show the data table */}
       <div>
         {/* This Div defines the Table Column Titles */}
-        <div className="flex gap-6 bg-gray-500 border rounded-lg text-white h-10 items-center">
-          {showColumn.title && <div className="w-28 text-center">Title</div>}
-          {showColumn.categories && (
-            <div className="w-28 text-center">Categories</div>
+        <div className="flex gap-6 bg-gray-500 border rounded-lg text-white h-10 items-center md:gap-3 md:w-4/5 sm:gap-1 sm:w-full">
+          {showColumn.title && (
+            <div className="w-28 md:w-20 text-center">Title</div>
           )}
-          {showColumn.price && <div className="w-28 text-center">Price</div>}
-          {showColumn.date && <div className="w-28 text-center">Date</div>}
-          {showColumn.author && <div className="w-28 text-center">Author</div>}
-          {showColumn.status && <div className="w-28 text-center">Status</div>}
-          {showColumn.action && <div className="w-28 text-center">Action</div>}
+          {showColumn.categories && (
+            <div className="w-28 md:w-20 text-center">Categories</div>
+          )}
+          {showColumn.price && (
+            <div className="w-28 md:w-20 text-center">Price</div>
+          )}
+          {showColumn.date && (
+            <div className="w-28 md:w-20 text-center">Date</div>
+          )}
+          {showColumn.author && (
+            <div className="w-28 md:w-20 text-center">Author</div>
+          )}
+          {showColumn.status && (
+            <div className="w-28 md:w-20 text-center">Status</div>
+          )}
+          {showColumn.action && (
+            <div className="w-28 md:w-20 text-center">Action</div>
+          )}
         </div>
         {data.map((rowData, index) => (
           // Mapping on the DataArray and put them in the column fields
-          <div key={index} className="flex gap-6 mt-2">
+          <div key={index} className="flex gap-6 mt-2 md:gap-3 md:w-4/5">
             {showColumn.title && (
-              <div className="w-28 text-center">{rowData.title}</div>
+              <div className="lg:w-28 md:w-20 md:text-sm text-center">
+                {rowData.title}
+              </div>
             )}
             {showColumn.categories && (
-              <div className="w-28 text-center">{rowData.categories}</div>
+              <div className="lg:w-28 md:w-20 sm:w-16 md:text-sm sm:text-sm text-center">
+                {rowData.categories}
+              </div>
             )}
             {showColumn.price && (
-              <div className="w-28 text-center">{rowData.price}</div>
+              <div className="lg:w-28 md:w-20 sm:w-16 md:text-sm sm:text-sm text-center">
+                {rowData.price}
+              </div>
             )}
             {showColumn.date && (
-              <div className="w-28 text-center">{rowData.date}</div>
+              <div className="lg:w-28 md:w-20 sm:w-16 md:text-sm sm:text-sm text-center">
+                {rowData.date}
+              </div>
             )}
             {showColumn.author && (
-              <div className="w-28 text-center">{rowData.author}</div>
+              <div className="lg:w-28 md:w-20 sm:w-16 md:text-sm sm:text-sm text-center">
+                {rowData.author}
+              </div>
             )}
             {showColumn.status && (
-              <div className="w-28 text-center">{rowData.status}</div>
+              <div className="lg:w-28 md:w-20 sm:w-16 md:text-sm sm:text-sm text-center">
+                {rowData.status}
+              </div>
             )}
             {showColumn.action && (
-              <div className="w-28 text-center">
+              <div className="lg:w-28 md:w-20 sm:w-16 md:text-sm sm:text-sm text-center">
                 <button className="bg-green-500 rounded-lg p-1">
                   {rowData.status}
                 </button>
@@ -73,14 +97,14 @@ const Table = ({ data }) => {
           </div>
         ))}
       </div>
-      <div className="">
+      <div>
         <button onClick={handleCheckbox}>
           <GiHamburgerMenu />
         </button>
 
         {/* Checkbox List*/}
         {showCheckBox && (
-          <div className="grid grid-cols-1 justify-items-start w-32 gap-3 text-lg">
+          <div className="grid grid-cols-1 justify-items-start w-32 gap-3 text-lg md:w-28 md:text-sm">
             <label>
               <input
                 type="checkbox"
