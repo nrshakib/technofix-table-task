7a30bc24f439c093911980426f99a9e682a25ef9
NRS\nrsha <nrshakib007@gmail.com>
2024-02-22 00:47:54 +0600

    Completed All Required  Functionalities

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,13 +5,13 @@ const Table = ({ data }) => {
   // state to set checkbox open or close
   const [showCheckBox, setShowCheckBox] = useState(false);
   const [showColumn, setShowColumn] = useState({
-    column1: true,
-    column2: true,
-    column3: true,
-    column4: true,
-    column5: true,
-    column6: true,
-    column7: true,
+    title: true,
+    categories: true,
+    price: true,
+    date: true,
+    author: true,
+    status: true,
+    action: true,
   });
 
   // function to set checkbox open or close
@@ -32,90 +32,108 @@ const Table = ({ data }) => {
       <div>
         {/* This Div defines the Table Column Titles */}
         <div className="flex gap-6 bg-gray-500 border rounded-lg text-white h-10 items-center">
-          <div className="w-28 text-center">Title</div>
-          <div className="w-28 text-center">Categories</div>
-          <div className="w-28 text-center">Price</div>
-          <div className="w-28 text-center">Date</div>
-          <div className="w-28 text-center">Author</div>
-          <div className="w-28 text-center">Status</div>
-          <div className="w-28 text-center">Action</div>
+          {showColumn.title && <div className="w-28 text-center">Title</div>}
+          {showColumn.categories && (
+            <div className="w-28 text-center">Categories</div>
+          )}
+          {showColumn.price && <div className="w-28 text-center">Price</div>}
+          {showColumn.date && <div className="w-28 text-center">Date</div>}
+          {showColumn.author && <div className="w-28 text-center">Author</div>}
+          {showColumn.status && <div className="w-28 text-center">Status</div>}
+          {showColumn.action && <div className="w-28 text-center">Action</div>}
         </div>
         {data.map((rowData, index) => (
           // Mapping on the DataArray and put them in the column fields
           <div key={index} className="flex gap-6 mt-2">
-            <div className="w-28 text-center">{rowData.title}</div>
-            <div className="w-28 text-center">{rowData.categories}</div>
-            <div className="w-28 text-center">{rowData.price}</div>
-            <div className="w-28 text-center">{rowData.date}</div>
-            <div className="w-28 text-center">{rowData.author}</div>
-            <div className="w-28 text-center">{rowData.status}</div>
-            <div className="w-28 text-center">
-              <button className="bg-green-500 rounded-lg p-1">
-                {rowData.action}
-              </button>
-            </div>
+            {showColumn.title && (
+              <div className="w-28 text-center">{rowData.title}</div>
+            )}
+            {showColumn.categories && (
+              <div className="w-28 text-center">{rowData.categories}</div>
+            )}
+            {showColumn.price && (
+              <div className="w-28 text-center">{rowData.price}</div>
+            )}
+            {showColumn.date && (
+              <div className="w-28 text-center">{rowData.date}</div>
+            )}
+            {showColumn.author && (
+              <div className="w-28 text-center">{rowData.author}</div>
+            )}
+            {showColumn.status && (
+              <div className="w-28 text-center">{rowData.status}</div>
+            )}
+            {showColumn.action && (
+              <div className="w-28 text-center">
+                <button className="bg-green-500 rounded-lg p-1">
+                  {rowData.status}
+                </button>
+              </div>
+            )}
           </div>
         ))}
       </div>
-      <div>
+      <div className="">
         <button onClick={handleCheckbox}>
           <GiHamburgerMenu />
         </button>
+
+        {/* Checkbox List*/}
         {showCheckBox && (
-          <div>
+          <div className="grid grid-cols-1 justify-items-start w-32 gap-3 text-lg">
             <label>
               <input
                 type="checkbox"
-                checked={showColumn.column1}
-                onChange={() => handleCheckBoxChange("column1")}
+                checked={showColumn.title}
+                onChange={() => handleCheckBoxChange("title")}
               />
               Title
             </label>
             <label>
               <input
                 type="checkbox"
-                checked={showColumn.column2}
-                onChange={() => handleCheckBoxChange("column2")}
+                checked={showColumn.categories}
+                onChange={() => handleCheckBoxChange("categories")}
               />
               Categories
             </label>
             <label>
               <input
                 type="checkbox"
-                checked={showColumn.column3}
-                onChange={() => handleCheckBoxChange("column3")}
+                checked={showColumn.price}
+                onChange={() => handleCheckBoxChange("price")}
               />
               Price
             </label>
             <label>
               <input
                 type="checkbox"
-                checked={showColumn.column4}
-                onChange={() => handleCheckBoxChange("column4")}
+                checked={showColumn.date}
+                onChange={() => handleCheckBoxChange("date")}
               />
               Date
             </label>
             <label>
               <input
                 type="checkbox"
-                checked={showColumn.column5}
-                onChange={() => handleCheckBoxChange("column5")}
+                checked={showColumn.author}
+                onChange={() => handleCheckBoxChange("author")}
               />
               Author
             </label>
             <label>
               <input
                 type="checkbox"
-                checked={showColumn.column6}
-                onChange={() => handleCheckBoxChange("column6")}
+                checked={showColumn.status}
+                onChange={() => handleCheckBoxChange("status")}
               />
               Status
             </label>
             <label>
               <input
                 type="checkbox"
-                checked={showColumn.column7}
-                onChange={() => handleCheckBoxChange("column7")}
+                checked={showColumn.action}
+                onChange={() => handleCheckBoxChange("action")}
               />
               Action
             </label>
